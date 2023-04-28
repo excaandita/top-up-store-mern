49070e789ac0a9520170cee559e984fc63ceb546
exca <excamuchlisandita@gmail.com>
2023-04-28 13:57:45 +0700

    complete refactor pages

--- components/organisms/Sidebar/MenuItem.tsx
+++ components/organisms/Sidebar/MenuItem.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import cx from "classnames";
+import Link from "next/link";
 
 interface MenuItemProps {
     title: string,
     icon: 'ic-menu-overview' | 'ic-menu-transaction' | 'ic-menu-reward' | 'ic-menu-messages' | 'ic-menu-card' | 'ic-menu-settings' | 'ic-menu-logout' ,
+    href: string,
     active?: boolean,
 }
 
 export default function MenuItem(props: Partial<MenuItemProps>) {
-    const { title, icon, active } = props;
+    const { title, icon, href, active } = props;
     const classItem = cx({
         'item': true,
         'mb-30': true,
@@ -18,10 +20,12 @@ export default function MenuItem(props: Partial<MenuItemProps>) {
         <>
             <div className={classItem}>
                 <div className="me-3">
-                    <Image src={`icon/${icon}.svg`} alt='ic-menu-overview' width={25} height={25}/>
+                    <Image src={`/icon/${icon}.svg`} alt='ic-menu-overview' width={25} height={25}/>
                 </div>
                 <p className="item-title m-0">
-                    <a href="" className="text-lg text-decoration-none">{title}</a>
+                    <Link href={`${href}`} legacyBehavior>
+                        <a className="text-lg text-decoration-none">{title}</a>
+                    </Link>
                 </p>
             </div>
         </>
